cd70f8d3baef19b12e32cf3523edba0c79b6578b
Zakkiy-Coding <muhammadzakkiy38@gmail.com>
2026-04-21 22:12:03 +0700

    implementasi tampilan trade market

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,53 @@
 import { ArrowUpIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {
+    HEATMAP_WIDGET_CONFIG,
+    MARKET_DATA_WIDGET_CONFIG,
+    MARKET_OVERVIEW_WIDGET_CONFIG,
+    TOP_STORIES_WIDGET_CONFIG
+} from "@/lib/constans";
 
 export default function Home() {
+    const scriptUrl = "https://s3.tradingview.com/external-embedding/embed-widget-";
   return (
-      <div className="flex flex-wrap items-center gap-2 md:flex-row">
-        <Button variant="outline">Button</Button>
-        <Button variant="outline" size="icon" aria-label="Submit">
-          <ArrowUpIcon />
-        </Button>
+      <div className="flex min-h-screen home-wrapper">
+        <section className="grid w-full gap-8 home-section">
+            <div className="md:col-span-1 xl:col-span-1">
+                <TradingViewWidget
+                  title="Market Overview"
+                  scriptUrl={scriptUrl + "market-overview.js"}
+                  config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                  className="custom-chart"
+                  height={600}
+                />
+            </div>
+            <div className="md-col-span xl:col-span-2">
+                <TradingViewWidget
+                    title="Stock Heat Map"
+                    scriptUrl={scriptUrl + "stock-heatmap.js"}
+                    config={HEATMAP_WIDGET_CONFIG}
+                    className="custom-chart"
+                    height={600}
+                />
+            </div>
+        </section>
+          <section className="grid w-full gap-8 home-section">
+            <div className="h-full md-col-span-1 xl:col-span-1">
+                <TradingViewWidget
+                  scriptUrl={scriptUrl + "timeline.js"}
+                  config={TOP_STORIES_WIDGET_CONFIG}
+                  height={600}
+                />
+            </div>
+            <div className="h-full md-col-span-1 xl:col-span-2">
+                <TradingViewWidget
+                    scriptUrl={scriptUrl + "market-quotes.js"}
+                    config={MARKET_DATA_WIDGET_CONFIG}
+                    height={600}
+                />
+            </div>
+        </section>
       </div>
   );
 }
